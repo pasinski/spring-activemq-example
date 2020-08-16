8d63c5406bb8ad79081309aab930902034b8dcf0
TribuneX <TribuneX@users.noreply.github.com>
2020-08-16 16:44:22 +0200

    Initial project structure

---
+++ src/test/java/me/bleidner/messaging/MessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bleidner.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
