97eb344edd8dbbb5dc3ec7de85303895ffcaa858
Michal Pasinski <michal.pasinski-extern@deutschebahn.com>
2020-08-18 10:01:17 +0200

    Add NotificationMessage class

---
+++ src/main/java/me/bleidner/messaging/model/NotificationMessage.java
@@ -0,0 +1,13 @@
+package me.bleidner.messaging.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationMessage {
+
+    private String content;
+}
