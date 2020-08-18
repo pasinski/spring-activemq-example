b0e51fe94b7993a83a09a96f247b6ff7b492fbcb
Michal Pasinski <michal.pasinski-extern@deutschebahn.com>
2020-08-18 10:40:30 +0200

    test redelivery

--- src/test/java/me/bleidner/messaging/MessageReceivedTest.java
+++ src/test/java/me/bleidner/messaging/MessageReceivedTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.jms.core.JmsTemplate;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
@@ -19,9 +22,24 @@ public class MessageReceivedTest {
     @MockBean
     private MessageProcessor messageProcessor;
 
+    @SpyBean
+    private MessageReceiver messageReceiver;
+
 
     @Test
     void shouldRecevieMessage() throws InterruptedException {
+        String message = "my message";
+        jmsTemplate.convertAndSend("myQueue", new NotificationMessage(message));
+
+        verify(messageProcessor, timeout(2000)).processMessage(message);
+    }
+
+    @Test
+    void shouldRedeliverMessage() {
+        doThrow(new RuntimeException("Exception in test"))
+                .doCallRealMethod()
+                .when(messageReceiver).receiveMessage(any());
+
         String message = "my message";
         jmsTemplate.convertAndSend("myQueue", new NotificationMessage(message));
 
